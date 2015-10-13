0b2813822774edf0216639d8ac74234c8a7392fb
Chris Smith <smith@terminallabs.com>
2015-10-13 18:46:23 -0400

    [B-09848] Trying to get image rendering params from django settings first.

--- lazythumbs/__init__.py
+++ lazythumbs/__init__.py
@@ -1,9 +1,15 @@
 __version__ = '0.4.4'
 
+try:
+    from django.conf import settings
+    has_settings = True
+except ImportError:
+    has_settings = False
 
-# NOTE: These are the default paramaters used for processing iamges with PIL.
+DEFAULT_QUALITY_FACTOR = settings.DEFAULT_QUALITY_FACTOR if has_settings else 80
+DEFAULT_OPTIMIZE_FLAG = settings.DEFAULT_OPTIMIZE_FLAG if has_settings else True
+DEFAULT_PROGRESSIVE_FLAG = settings.DEFAULT_PROGRESSIVE_FLAG if has_settings else True
 
-DEFAULT_QUALITY_FACTOR = 80
-DEFAULT_OPTIMIZE_FLAG = True
-DEFAULT_PROGRESSIVE_FLAG = True
+
+
 
