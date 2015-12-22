af069ee854951de6911e935ffb8385f57e4b64e3
Chris Smith <smith@terminallabs.com>
2015-12-22 14:42:33 -0500

    [B-09850] Simplifying django settings imports.

--- lazythumbs/__init__.py
+++ lazythumbs/__init__.py
@@ -1,40 +1,16 @@
 __version__ = '0.4.9'
-
 import logging
 logger = logging.getLogger('lazythumbs')
-
-
 # NOTE: These are the default paramaters used by lazythumbs for processing images with PIL. 
 #       We will try to get them from django settings, and if that fails we can use these
 #       fallback values.
+
+from django.conf import settings
 
 fallback_quality_factor = 80
 fallback_optimize_flag = True
 fallback_progressive_flag = True
 
-try:
-    from django.conf import settings
-
-    try:
-        DEFAULT_QUALITY_FACTOR = settings.DEFAULT_QUALITY_FACTOR
-    except AttributeError:
-        logger.debug("Failed to retrieve DEFAULT_QUALITY_FACTOR from django settings, it may not be set")
-
-    try:
-        DEFAULT_OPTIMIZE_FLAG = settings.DEFAULT_OPTIMIZE_FLAG
-    except AttributeError:
-        logger.debug("Failed to retrieve DEFAULT_OPTIMIZE_FLAG from django settings, it may not be set")
-
-    try:
-        DEFAULT_PROGRESSIVE_FLAG = settings.DEFAULT_PROGRESSIVE_FLAG
-    except AttributeError:
-        logger.debug("Failed to retrieve DEFAULT_PROGRESSIVE_FLAG from django settings, it may not be set")
-
-except ImportError:
-    logger.debug("Failed to import django settings")
-
-finally:
-    DEFAULT_QUALITY_FACTOR = DEFAULT_QUALITY_FACTOR if 'DEFAULT_QUALITY_FACTOR' in locals() else fallback_quality_factor
-    DEFAULT_OPTIMIZE_FLAG = DEFAULT_OPTIMIZE_FLAG if 'DEFAULT_OPTIMIZE_FLAG' in locals() else fallback_optimize_flag
-    DEFAULT_PROGRESSIVE_FLAG = DEFAULT_PROGRESSIVE_FLAG if 'DEFAULT_PROGRESSIVE_FLAG' in locals() else fallback_progressive_flag
-
+DEFAULT_QUALITY_FACTOR = getattr(settings, 'LAZYTHUMBS_QUALITY_FACTOR', fallback_quality_factor)
+DEFAULT_OPTIMIZE_FLAG = getattr(settings, 'LAZYTHUMBS_OPTIMIZE_FLAG', fallback_optimize_flag)
+DEFAULT_PROGRESSIVE_FLAG = getattr(settings, 'LAZYTHUMBS_PROGRESSIVE_FLAG', fallback_progressive_flag)
